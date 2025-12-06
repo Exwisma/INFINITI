cb9e9d7deba4870f36cb7f01e209e8f4ea015274
Exwisma <ismailabbasov900@gmail.com>
2025-12-06 20:51:10 +0500

    удалил фотки ловайбла

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="py-12 px-6 border-t border-border">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
         <div className="text-2xl font-light tracking-wider">
-          INFINITY
+          INFINITI
         </div>
         
         <div className="text-sm text-muted-foreground font-light">
